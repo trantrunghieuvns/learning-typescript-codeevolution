f588c426e7f2c643833d15b768ab8b5e213cc855
trantrunghieuvns <trantrunghieuvns@gmail.com>
2022-11-13 18:12:33 +0700

    useReducer 2

--- src/state/useReducer_Example/DispatchReducerAction.tsx
+++ src/state/useReducer_Example/DispatchReducerAction.tsx
@@ -1,5 +1,8 @@
 import React, { useReducer } from 'react'
-import { initialState, ReducerExample } from './ReducerAction'
+import { ReducerExample } from './ReducerAction'
+import { initialState } from './ReducerAction'
+
+
 
 export const ReducerCounter = () => {
     const [state, dispatch] = useReducer(ReducerExample, initialState)
@@ -7,12 +10,20 @@ export const ReducerCounter = () => {
         <>
             Count: {state.count}
             {/*  receive action.type from reducerAction > get type and write it down here */}
-            <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+            <button className='border-2 bg-gray-500 p-1 ' onClick={() => dispatch({ type: 'increment', payload: 10 })}>
                 Increment 10
             </button>
-            <button onClick={() => dispatch({ type: 'decrement', payload: 4 })}>
+            <button className='border-2 bg-gray-500 p-1' onClick={() => dispatch({ type: 'decrement', payload: 4 })}>
                 Decrement 8
             </button>
+
+            <button className='border-2 bg-gray-500 p-1 ' onClick={() => dispatch({ type: 'reset', payload: 0 })}>
+                reset all
+            </button>
+            <div>
+                {state.count === 0 ? 'hết rồi nhé' : 'chưa chắc đã hết đâu'}
+            </div>
+
         </>
     )
 }
--- src/state/useReducer_Example/ReducerAction.tsx
+++ src/state/useReducer_Example/ReducerAction.tsx
@@ -1,19 +1,25 @@
 type CounterState = {
     count: number
+
 }
 type CounterAction = {
-    type: string
+    type: 'increment' | 'decrement' | 'reset' // its string beforehand 
     payload: number
 }
 
 export const initialState = { count: 0 }
 
 export function ReducerExample(state: CounterState, action: CounterAction) {
-    switch (action.type) { // give this action.type => type in dispatchAction file
+    switch (action.type) { // give this action.type types in dispatchAction file
         case 'increment':
             return { count: state.count + action.payload }
         case 'decrement':
-            return { count: state.count - action.payload }
+            return { count: state.count + action.payload }
+        case 'reset':
+            return {
+                count: state.count * 0,
+                payload: 0
+            }
 
         default:
             return state
