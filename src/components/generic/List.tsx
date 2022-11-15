c912fd69ec95b29c7d38071ef8314e3017436a36
trantrunghieuvns <trantrunghieuvns@gmail.com>
2022-11-15 12:55:03 +0700

    reset all button completed

--- src/components/generic/List.tsx
+++ src/components/generic/List.tsx
@@ -4,7 +4,6 @@ import { uuid } from 'uuidv4';
 
 type ListProps = {
     index: number
-
     item: string
     arrayB: string[]
     items: string[]
@@ -14,48 +13,41 @@ type ListProps = {
 
 export const List = ({ items, onClick }: ListProps) => {
     let [phuItems, setPhuItems] = useState<string[]>([...items]);
-    let [removedItems, setRemovedItems] = useState<string[]>([]);
-    // listItems = items
-
-
-    let arrayB: string[] = [];
+    // let [removedItems, setGetRemovedItems] = useState<string[]>([]); *1*
     let [listItems, setListItems] = useState<string[]>(items);
 
-    console.log('listItems :>> ', listItems);
     const deleteItem = (index: number) => {
-        setRemovedItems(listItems.filter((a, e) => index === e))
-
+        // setGetRemovedItems(listItems.filter((a, e) => index === e)) *2*
         listItems.splice(index, 1)
-        setListItems((listItems) => [...listItems])
-        console.log('listItems', listItems)
+        setListItems([...listItems])
     }
+
+    const handleClick = () => {
+        let uniqueString: string[] = [];
+        items.concat(phuItems).forEach((c) => {
+            if (!uniqueString.includes(c)) {
+                uniqueString.push(c);
+            }
+            setListItems(uniqueString);
+        });
+    }
+
     return (
         <>
             <div>
-                <h2>List of Items </h2>
+                <h2 className='p-3'>List of Items </h2>
                 {listItems.map((item, index) => {
-                    return (<>
-                        <div key={index + 1 + index} onClick={() => {
-                            deleteItem(index)
-
-                        }}>{item}</div>
-                    </>
+                    return (
+                        <>
+                            <div key={index} onClick={() => {
+                                deleteItem(index)
+                            }}>{item}</div>
+                        </>
                     )
                 })
                 }
-                <div onClick={() => {
-
-                    let longArray = items.concat(removedItems).concat(phuItems)
-                    let uniqueString: string[] = [];
-                    longArray.forEach((c) => {
-                        if (!uniqueString.includes(c)) {
-                            uniqueString.push(c);
-                        }
-                    });
-                    setListItems(uniqueString)
-                }}>Reset All</div>
-
-
+                <div className='bg-gray-400 rounded-lg p-3' onClick={handleClick //or () => handleClick()
+                }>Reset All</div>
             </div >
         </>
     )
